2e2a2b3084b27b06343aebf8b0a41e6d4a80869c
sebaro <esebaro@gmail.com>
2018-02-06 10:40:17 +0200

    Remove Old Extensions

--- Maxthon/SaveTube/page.js
+++
@@ -1,4 +0,0 @@
-javascript:
-var myRuntime = window.external.mxGetRuntime();
-var mxTabs = myRuntime.create('mx.browser.tabs');
-var TabNum = mxTabs.newTab({url:"http://isebaro.com/savetube",activate: true,position:"afterCurrrent"});
