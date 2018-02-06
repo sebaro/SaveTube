f47d1f174bef405393b3cc4d20b5005caa932ee2
sebaro <esebaro@gmail.com>
2018-02-06 10:49:41 +0200

    Update Extensions

---
+++ Extensions/Maxthon/SaveTube/page.js
@@ -0,0 +1,4 @@
+javascript:
+var myRuntime = window.external.mxGetRuntime();
+var mxTabs = myRuntime.create('mx.browser.tabs');
+var TabNum = mxTabs.newTab({url:"http://isebaro.com/savetube",activate: true,position:"afterCurrrent"});
